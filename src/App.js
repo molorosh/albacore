199a89c6b1df84f31aa21cf23bfa0aa5c4e815bd
Howard G Ricketts <molorosh@gmail.com>
2020-02-21 18:35:06 +0000

    making some changes from the basic template app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          <ruby>鬢長 <rp>(</rp><rt>びんなが</rt><rp>)</rp></ruby>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
