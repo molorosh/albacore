9b059e4f2ff7e6a97cb02d1a7a93f0a7078f993e
Howard G Ricketts <molorosh@gmail.com>
2020-02-22 16:08:41 +0000

    added ProtoAlpha section

---
+++ src/SectionProtoAlpha.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SectionProtoAlpha(props){
+    return (
+        <section className="App-section">
+          <h2>Proto: ἄλφα</h2>
+          <p>TODO...</p>
+          <p></p>
+          <p>Under Construction</p>
+        </section>
+    );
+}
+
+export default SectionProtoAlpha;
